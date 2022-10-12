98b67eca6458470eb0d36724a6ca375026ac07a3
Jared Mejia <jaredmejia4440@gmail.com>
2022-10-11 21:44:35 -0600

    send emails with node done

---
+++ Server/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+// Routers
+const { emailsRouter } = require("./routes/emails.routes");
+
+// Controllers
+const { globalErrorHandler } = require("./controllers/error.controller");
+
+// Init our Express app
+const app = express();
+
+// Enable Express app to receive JSON data
+app.use(express.json());
+
+app.use(helmet());
+
+app.use(compression());
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else if (process.env.NODE_ENV === "production") app.use(morgan("combined"));
+
+// Enable cors
+app.use(cors());
+
+// Define endpoints
+app.use("/api/v1/emails", emailsRouter);
+
+// Global error handler
+app.use(globalErrorHandler);
+
+// Catch non-existing endpoints
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: `${req.method} ${req.url} does not exists in our server`,
+  });
+});
+
+module.exports = { app };
---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+
+const { app } = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const startServer = async () => {
+  try {
+    // Set server to listen
+    const PORT = 4000;
+
+    app.listen(PORT, () => {
+      console.log("Express app running!");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
