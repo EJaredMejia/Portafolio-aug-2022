9a4c59d53f31ba7e040b9e7b2cc9000b53280e88
Jared Mejia <emejia@ingenieria.digital>
2025-02-21 16:47:49 -0600

    feat: migrate to tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from "vite";
-import path, { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: path.join(__dirname, "../server/dist"),
-    emptyOutDir: true,
   },
 });
