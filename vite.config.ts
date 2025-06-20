b94cb16f92af714b8e63d7688074f8207ab88c3e
Jared Mejia <emejia@ingenieria.digital>
2025-06-20 11:08:25 -0600

    feat: migration to devinxi start

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
 import tailwindcss from "@tailwindcss/vite";
 import { dirname, resolve } from "path";
-import { defineConfig } from "@tanstack/react-start/config";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = dirname(__filename); // get the name of the directory
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  tsr: {
-    appDirectory: 'app'
-  },
-  vite: {
-    plugins: [
-      tailwindcss(),
-      tsConfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "./app"),
-      },
+  plugins: [
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      target: "netlify",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
